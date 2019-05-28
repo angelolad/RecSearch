9935cb487b954713631a9d3b16b58fa8fcad6776
angelolad <angelo.ladores@outlook.com>
2019-05-27 23:28:33 -0400

    Updated recipe view

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -61,26 +62,35 @@ const controlRecipe = async () => {
     //Get ID from url
     //Replace the hashing symbol with nothing so we only have the id
     const id = window.location.hash.replace('#', '');
-    console.log(id);
 
     if (id) {
         // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
+        //Highlight selected seaerch item
+        if (state.search){
+            searchView.highlightSelected(id);
+        }
+        
         // Create new recipe object
         state.recipe = new Recipe(id);
         try {
             // Get recipe data
             await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
 
             // Calculate servings and time
             state.recipe.calcTime();
             state.recipe.calcServings();
 
             // Render recipe 
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         }
+
         catch (err){
-            alert(`Error processign recipe!`);
+            alert(`Error processing recipe!`);
         }
      
     }
